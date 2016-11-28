6751811ee3f9babfd5648bfb0e0c47c2d89dc1e3
Brian Haas <brianjohnhaas@gmail.com>
2016-11-28 12:56:14 -0500

    pull longest isoform per transcript

--- test_LongReads/partition_target_transcripts.pl
+++ test_LongReads/partition_target_transcripts.pl
@@ -18,6 +18,10 @@ my $ref_trans_fa;
 my $MIN_REFSEQ_LENGTH = 100;
 my $OUT_DIR = "Seqs_dir";
 
+my $MAX_ISOFORMS = -1;
+my $MIN_ISOFORMS = 2;
+
+
 my $usage = <<__EOUSAGE__;
 
 ################################################################################
@@ -38,7 +42,12 @@ my $usage = <<__EOUSAGE__;
 #  --min_refseq_length <int>            min length for a reference transcript 
 #                                       sequence (default: $MIN_REFSEQ_LENGTH)
 #
-#  --max_isoforms <int>                 max number of isoforms to test
+#  if --by_Gene:
+#
+#    --min_isoforms <int>                 default: $MIN_ISOFORMS
+#    --max_isoforms <int>                 max number of isoforms to test (default: $MAX_ISOFORMS)
+#
+#    --longest_isoform_only               restricts to only single longest isoform per gene.
 #
 ############################################################################################
 
@@ -50,22 +59,24 @@ __EOUSAGE__
 
 
 my $BY_GENE_FLAG = 0;
-my $MAX_ISOFORMS = -1;
-
-
+my $LONGEST_ISOFORM_ONLY_FLAG = 0;
 
 &GetOptions ( 'h' => \$help_flag,
-              
-
+                            
               # required
               'ref_trans|R=s' => \$ref_trans_fa,
               
               # optional
               'out_dir|O=s' => \$OUT_DIR,
               'min_refseq_length=i' => \$MIN_REFSEQ_LENGTH,
-    
+              
               'by_Gene' => \$BY_GENE_FLAG,
-
+              
+              'max_isoforms=i' => \$MAX_ISOFORMS,
+              'min_isoforms=i' => \$MIN_ISOFORMS,
+              
+              'longest_isoform_only' => \$LONGEST_ISOFORM_ONLY_FLAG,
+              
 );
 
 
@@ -119,7 +130,9 @@ main: {
         my @min_length_targets;
         foreach my $entry (@$seq_entries_aref) {
             
-            my ($trans_acc, $seq) = @$entry;
+            my ($trans_acc, $seq) = ($entry->{acc},
+                                     $entry->{seq});
+            
             if (length($seq) >= $MIN_REFSEQ_LENGTH) {
                 push (@min_length_targets, $entry);
             }
@@ -130,18 +143,37 @@ main: {
             next; 
         }
         
-
+        @min_length_targets = reverse sort {length($a->{seq}) <=> length($b->{seq}) } @min_length_targets;
+        
         my $num_total_targets = scalar(@min_length_targets);
         
-        if ($BY_GENE_FLAG && $num_total_targets < 2) {
-            next;
+        if ($BY_GENE_FLAG) {
+            
+            if ($LONGEST_ISOFORM_ONLY_FLAG) {
+                @min_length_targets = shift @min_length_targets;
+            }
+            else {
+                
+                if ($num_total_targets < $MIN_ISOFORMS) {
+                    next;
+                }
+                
+                if ($num_total_targets > $MAX_ISOFORMS) {
+                    
+                    @min_length_targets = @min_length_targets[0..($num_total_targets-1)];
+                }
+            }
+            
         }
+        
+
+
 
         &prep_seqs($acc, \@min_length_targets);
         
         $total_counter++;
         if ($total_counter % 100 == 0) {
-            print STDERR "[$total_counter]\n\n";
+            print STDERR "\n[$total_counter]\n";
         }
     }
     
@@ -156,7 +188,7 @@ main: {
 sub prep_seqs {
     my ($acc, $entries_aref) = @_;
     
-    print STDERR "-processing $acc\n";
+    print STDERR "\r-processing $acc   ";
 
     my $num_entries = scalar(@$entries_aref);
 
@@ -177,8 +209,10 @@ sub prep_seqs {
     if (! -s "$refseqs_fa") {
         open (my $ofh, ">$refseqs_fa") or die $!;
         foreach my $entry (@$entries_aref) {
-            my ($entry_acc, $seq_acc) = @$entry;
-            print $ofh ">$entry_acc\n$seq_acc\n";
+            my ($entry_acc, $seq) = ($entry->{acc},
+                                     $entry->{seq});
+            
+            print $ofh ">$entry_acc\n$seq\n";
         }
         close $ofh;
     }
@@ -229,16 +263,11 @@ sub reorganize_fasta_seqs {
             
         }
         
-        if ($MAX_ISOFORMS > 1 && exists $reorg_fasta{$key}) {
-            
-            if (scalar(@{$reorg_fasta{$key}} >= $MAX_ISOFORMS)) {
-                next;
-            }
-        }
-        
-        push (@{$reorg_fasta{$key}}, [$acc, $seq]);
+        push (@{$reorg_fasta{$key}}, { acc => $acc, 
+                                       seq => $seq}
+            );
     }
-
+    
     return(%reorg_fasta);
     
 }
