df39f89c8641169d5a6316fb0ed32c032551ee69
Brian Haas <brianjohnhaas@gmail.com>
2018-09-25 09:14:51 -0400

    add

--- examine_read_kmer_cov_vs_seq_errors/plot_pair_stats_w_errors.R
+++ examine_read_kmer_cov_vs_seq_errors/plot_pair_stats_w_errors.R
@@ -9,3 +9,11 @@ df$num_errors = as.factor(df$num_errors)
 
 df %>% ggplot(aes(x=meanval, y=stdev, color=num_errors)) + geom_point(alpha=1/2)
 
+df %>% filter(as.numeric(num_errors) < 5) %>% ggplot(aes(x=log(meanval+.1), y=log( (stdev+0.1) / (meanval+.1) ), color=num_errors)) + geom_point(alpha=1/2) + ylim(-4, 2)
+
+
+df %>% filter(as.numeric(num_errors) < 5) %>% ggplot(aes(log( (stdev) / (meanval) ), color=num_errors)) + geom_density(alpha=0.1) + xlim(-4, 2.5)
+
+df %>% filter(as.numeric(num_errors) < 10) %>% ggplot(aes(( (stdev) / (meanval) ), color=num_errors, fill=num_errors)) + geom_density(alpha=0.1) + xlim(0, 2.5)
+
+
